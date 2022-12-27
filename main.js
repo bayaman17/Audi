3ddc98c8aca73c859ff758ffd41e3cd03d3fc785
bayaman17 <bayamanborubaev255@gmail.com>
2022-12-27 21:12:45 +0600

    burger menu

--- main.js
+++ main.js
@@ -18,3 +18,18 @@ $(document).ready(function(){
       }
     }
   });
+  let btn = document.querySelector('.mobile-btn')
+
+  const burgerFunc = (menuClassName, showClassName) =>{
+    let menu = document.querySelector(`.${menuClassName}`)
+
+    if (menu.className.includes(showClassName)){
+      menu.className = menuClassName;
+    }else {
+      menu.className += ` ${showClassName}`;
+    }
+  };
+
+  btn.addEventListener('click', ()=>{
+    burgerFunc('header__menu', 'header__menu-show');
+  });
